040d01958deb8b519d243dd8491632c227628d3e
Douglas Gomes <douglasgomes.rr@gmail.com>
2026-01-17 01:59:16 -0300

    feat: enhance environment configuration and update dependencies
    - Added new remote URLs to .env.example and .env.production for better integration.
    - Updated package.json to include cross-env for environment variable management in scripts.
    - Enhanced package-lock.json with new dependencies and updated versions for better compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,51 @@
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { z } from "zod";
 
-export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		federation({
-			name: "@bytebank/financeiro",
-			filename: "remoteEntry.js",
-			exposes: {
-				"./bytebank-financeiro": "./src/exposes/bytebank-financeiro.tsx",
-			},
-			remotes: {
-				"@bytebank/root": "http://localhost:9000/assets/remoteEntry.js",
-			},
-			shared: ["react", "react-dom", "react-router-dom"],
-		}),
-	],
-	build: {
-		modulePreload: false,
-		target: "esnext",
-		minify: false,
-		cssCodeSplit: false,
-	},
-	server: {
-		port: 9000,
-		fs: {
-			allow: ["."],
+const schema = z.object({
+	VITE_API_BASE_URL: z.url(),
+	VITE_REMOTE_ROOT_URL: z.url(),
+	VITE_REMOTE_FINANCEIRO_URL: z.url(),
+	VITE_REMOTE_DASHBOARD_URL: z.url(),
+	VITE_REMOTE_BASE_URL: z.url(),
+});
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	const ENV = schema.parse(loadEnv(mode, process.cwd(), ""));
+
+	return {
+		plugins: [
+			react(),
+			tailwindcss(),
+			federation({
+				name: "@bytebank/financeiro",
+				filename: "remoteEntry.js",
+				exposes: {
+					"./bytebank-financeiro": "./src/exposes/bytebank-financeiro.tsx",
+				},
+				remotes: {
+					"@bytebank/root": ENV.VITE_REMOTE_ROOT_URL,
+				},
+				shared: ["react", "react-dom", "react-router-dom"],
+			}),
+		],
+		build: {
+			modulePreload: false,
+			target: "esnext",
+			minify: false,
+			cssCodeSplit: false,
 		},
-	},
-	preview: {
-		port: 9002,
-	},
+		server: {
+			port: 9000,
+			fs: {
+				allow: ["."],
+			},
+		},
+		preview: {
+			port: 9002,
+		},
+	};
 });
